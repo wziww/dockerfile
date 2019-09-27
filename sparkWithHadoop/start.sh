3d43be5cfaebd422b0bd2aced61819768964463f
wziww <maoshuai@maoshuaideMacBook-Pro.local>
2019-09-27 13:57:22 +0800

    init

--- sparkWithHadoop/start.sh
+++ sparkWithHadoop/start.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
-/usr/sbin/sshd -D & 
-sleep 3;
-$HADOOP_PREFIX/etc/hadoop/hadoop-env.sh && $HADOOP_PREFIX/sbin/start-dfs.sh && $HADOOP_PREFIX/bin/hadoop fs -mkdir -p /user/root
+/usr/sbin/sshd -D &
+sleep 3
+>$HADOOP_PREFIX/etc/hadoop/slave
+$HADOOP_PREFIX/etc/hadoop/hadoop-env.sh
+$HADOOP_PREFIX/bin/hdfs namenode -format
+if [ $(hostname -s) = "master" ]; then
+  for i in $(echo $slave | tr ";" "\n"); do
+    $i >>$HADOOP_PREFIX/etc/hadoop/slave
+  done
+  $HADOOP_PREFIX/sbin/start-all.sh
+fi
 /bin/bash -c "$*"
