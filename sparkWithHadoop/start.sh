05d23e6860892bdf544ba61298c4f743effb1576
wziww <maoshuai@maoshuaideMacBook-Pro.local>
2019-09-27 08:52:04 +0800

    init

--- sparkWithHadoop/start.sh
+++ sparkWithHadoop/start.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 /usr/sbin/sshd -D & 
 sleep 3;
-$HADOOP_PREFIX/etc/hadoop/hadoop-env.sh && $HADOOP_PREFIX/sbin/start-dfs.sh && $HADOOP_PREFIX/bin/hdfs dfs -mkdir -p /user/root
+$HADOOP_PREFIX/etc/hadoop/hadoop-env.sh && $HADOOP_PREFIX/sbin/start-dfs.sh && $HADOOP_PREFIX/bin/hadoop fs -mkdir -p /user/root
